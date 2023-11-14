f174f6d5e4cd074b8772112aa6192695503fe259
jwjung1108 <jwjung1108@naver.com>
2023-11-14 14:56:24 +0900

    a

--- index.php
+++ index.php
@@ -193,13 +193,14 @@ session_start();
             <?php } else { ?>
                 <button class="btn btn-outline-light" onclick="goToLoginPage()">로그인</button>
                 <button class="btn btn-outline-light" onclick="goToSignupPage()">회원가입</button>
+                <div style="float:right;">
+                    <button class="btn btn-outline-light" onclick="goToadminPage()">관리자페이지</button>
+                </div>
             <?php } ?>
         </div>
     </div>
 </nav>
-<div style="float:right;">
-    <button class onclick="goToadminPage()">관리자페이지</button>
-</div>
+
 
 <!-- Main Content -->
 <div class="container mt-4">
