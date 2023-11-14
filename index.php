1f9577bb13836b6fc11dce96c41bf445bcfe5b73
underkim <rlaehdgns4568@gmail.com>
2023-11-14 12:17:40 +0900

    main page change version

--- index.php
+++ index.php
@@ -88,6 +88,69 @@ session_start();
             justify-content: center;
             margin-bottom: 30px;
         }
+        .navbar-custom {
+    background-color: #0056b3;
+    padding: 10px 0;
+    color: white;
+}
+
+.nav-link,
+.navbar-brand {
+    color: white;
+    margin-right: 15px;
+}
+
+.nav-link:hover,
+.navbar-brand:hover {
+    color: #f2f2f2;
+}
+
+/* 섹션 타이틀 스타일링 */
+.section-title {
+    margin-top: 20px;
+    margin-bottom: 10px;
+    color: #333;
+    text-align: left;
+    font-size: 24px;
+    font-weight: 600;
+}
+
+/* 카드 스타일링 */
+.card-custom {
+    margin: 10px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+}
+
+.card-custom img {
+    width: 100%;
+    height: auto;
+    border-top-left-radius: calc(.25rem - 1px);
+    border-top-right-radius: calc(.25rem - 1px);
+}
+
+.card-body-custom {
+    padding: 10px;
+}
+
+.card-title {
+    margin-bottom: 10px;
+    font-weight: bold;
+}
+
+.card-text {
+    font-size: 14px;
+    color: #666;
+}
+
+.card-link {
+    font-size: 16px;
+    color: #0056b3;
+}
+
+.card-link:hover {
+    text-decoration: none;
+    color: #003d82;
+}
     </style>
     <script>
         function goToLoginPage() {
@@ -100,10 +163,7 @@ session_start();
             window.location.href = "/board/list_board.php";
         }
         function goTonotificationBoardPage() {
-            window.location.href = "/board/notification/list_nboard.php";
-        }
-        function goToQandAPage() {
-            window.location.href = "/board/QandA/list_qboard.php";
+            window.location.href = "/board/list_nboard.php";
         }
         function logout() {
             const data = confirm("로그아웃 하시겠습니까?");
@@ -115,28 +175,92 @@ session_start();
 </head>
 
 <body>
-    <div class="container">
-        <div class="welcome-container">
-            <?php if (isset($_SESSION['userId'])) { ?>
-                <div class="welcome-message">
-                    <?php echo $_SESSION['userName']; ?> 님 환영합니다
-                </div>
-                <span class="logout-button" onclick="logout()">로그아웃</span>
-            <?php } else { ?>
-                <button onclick="goToLoginPage()">로그인</button>
-                <button onclick="goToSignupPage()">회원가aaaaa입</button>
-            <?php } ?>
-        </div>
-        <h1>홈페이지</h1>
-        <div class="board-buttons">
-            <?php if (isset($_SESSION['userId'])) { ?>
-                <button onclick="goTocommonBoardPage()">자유게시판</button>
-            <?php } ?>
-            <button onclick="goTonotificationBoardPage()">공지사항</button>
-            <button onclick="goToQandAPage()">Q&A</button>
 
+ <!--- GPT 코드 주석--->
+
+<nav class="navbar navbar-custom">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">학습 커뮤니티</a>
+        <div class="d-flex align-items-center">
+            <?php if (isset($_SESSION['userId'])) { ?>
+                <span class="navbar-text mr-3">
+                    <?php echo $_SESSION['userId']; ?> 님 환영합니다
+                </span>
+                <button class="btn btn-outline-light" onclick="logout()">로그아웃</button>
+            <?php } else { ?>
+                <button class="btn btn-outline-light" onclick="goToLoginPage()">로그인</button>
+                <button class="btn btn-outline-light" onclick="goToSignupPage()">회원가입</button>
+            <?php } ?>
         </div>
     </div>
+</nav>
+
+
+<!-- Main Content -->
+<div class="container mt-4">
+
+    <!-- Section Title -->
+    <div class="section-title">
+        주요 기능
+    </div>
+    
+    <!-- Features -->
+    <div class="row">
+        <!-- 강의 평가 -->
+        <div class="col-md-3">
+            <div class="card card-custom">
+                <img src="/image/1.webp" alt="자유게시판">
+                <div class="card-body card-body-custom">
+                    <h5 class="card-title">자유게시판</h5>
+                    <p class="card-text">강의에 대한 리뷰와 평점을 남기고 공유할 수 있습니다.</p>
+                    <?php if (isset($_SESSION['userId'])) { ?>
+                <button onclick="goTocommonBoardPage()">자유계시판</button>
+            <?php } ?>
+                </div>
+            </div>
+        </div>
+        
+        <!-- 과제 공유 -->
+        <div class="col-md-3">
+            <div class="card card-custom">
+                <img src="/image/1.webp" alt="과제 공유">
+                <div class="card-body card-body-custom">
+                    <h5 class="card-title">과제 공유</h5>
+                    <p class="card-text">과제 자료를 업로드하고 다른 사용자와 아이디어를 공유하세요.</p>
+                    
+                </div>
+            </div>
+        </div>
+        
+        <!-- 학습 자료실 -->
+        <div class="col-md-3">
+            <div class="card card-custom">
+                <img src="/image/1.webp" alt="학습 자료실">
+                <div class="card-body card-body-custom">
+                    <h5 class="card-title">학습 자료실</h5>
+                    <p class="card-text">다양한 학습 자료를 다운로드 받고, 자신만의 자료도 공유해보세요.</p>
+                    
+                </div>
+            </div>
+        </div>
+        
+        <!-- 토론 포럼 -->
+        <div class="col-md-3">
+            <div class="card card-custom">
+                <img src="/image/1.webp" alt="공지 사항">
+                <div class="card-body card-body-custom ">
+                    <h5 class="card-title">공지사항</h5>
+                    <p class="card-text">학업 관련 토론에 참여하고 지식을 공유하세요.</p>
+                    <button onclick="goTonotificationBoardPage()">공지사항</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
 </body>
 
 </html>
