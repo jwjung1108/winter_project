9c598940a0d192f3af49f7f6420613adeddd565d
underkim <rlaehdgns4568@gmail.com>
2023-11-15 13:18:48 +0900

    search_result.php modified

--- board/search_result.php
+++ board/search_result.php
@@ -1,44 +1,17 @@
-<?php
-include '../connect.php';
-
-// 정렬 방식 설정
-$sort = isset($_GET['sort']) ? $_GET['sort'] : 'number'; // 기본값은 순번
-$sortIcon = ($sort == 'number') ? '▲' : '▼';
-
-// 정렬 기준 설정
-$orderBy = '';
-switch ($sort) {
-    case 'views':
-        $orderBy = 'ORDER BY views';
-        break;
-    case 'likes':
-        $orderBy = 'ORDER BY likes';
-        break;
-    default:
-        $orderBy = 'ORDER BY number';
-        break;
-}
-
-// SQL 쿼리문 수정
-?>
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="ko">
 
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta charset="utf-8">
+    <title>게시판</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
         crossorigin="anonymous"></script>
-
-
-    <title>게시판</title>
     <style>
         body {
             padding-top: 50px;
@@ -53,82 +26,75 @@ switch ($sort) {
             color: #212529;
         }
 
-        .sortable {
-            cursor: pointer;
-        }
-
         h1 {
             margin-top: 30px;
-        }
-
-        .text-end {
-            margin-bottom: 20px;
         }
 
         #search_box {
             margin: 20px 0;
         }
+
+        /* 모바일 친화적인 스타일 */
+        @media (max-width: 768px) {
+            body {
+                padding-top: 20px;
+            }
+
+            .container {
+                padding: 10px;
+            }
+
+            h1 {
+                font-size: 24px;
+            }
+
+            .table {
+                font-size: 14px;
+            }
+
+            #search_box {
+                margin: 10px 0;
+            }
+        }
     </style>
 </head>
 
 <body>
-    <?php
-
-    $search_con = isset($_GET['search']) ? $_GET['search'] : '';
-    $category = isset($_GET['catgo']) ? $_GET['catgo'] : '';
-
-    // 선택한 카테고리 체크박스 값 가져오기
-    $selectedCategories = isset($_GET['category']) ? $_GET['category'] : array();
-
-    // 카테고리를 OR 연산으로 조합
-    $categoryCondition = '';
-    if (!empty($selectedCategories)) {
-        $categoryCondition = "AND (";
-        foreach ($selectedCategories as $selectedCategory) {
-            $categoryCondition .= "$selectedCategory = 1 OR ";
-        }
-        $categoryCondition = rtrim($categoryCondition, " OR ") . ")";
-    }
-
-
-    // $boardNames = array(
-    //     'freeboard' => '자유게시판',
-    //     'notification' => '공지사항',
-    //     'QandA' => 'Q&A'
-    // );
-
-    // SQL 쿼리문 수정
-    $sql = "SELECT * FROM board WHERE $category LIKE '%$search_con%' $categoryCondition AND isSecret = 0 $orderBy";
-    $result = mysqli_query($conn, $sql);
-
-
-
-    if ($category == 'title') {
-        $catname = '제목';
-    } else if ($category == 'username') {
-        $catname = '작성자';
-    } else if ($category == 'board') {
-        $catname = '내용';
-    }
-
-    ?>
-    <h1>
-        <?php echo $catname; ?>:
-        <?php echo $search_con; ?> 검색결과
-    </h1>
-
-
-    <h4 style="margin-top:30px;"><a href="../index.php">홈으로</a></h4>
-
-    <?php
-
-    ?>
-
-
     <div class="container">
-        <h1 class="text-center">게시판</h1>
+        <h1 class="text-center">학업 노하우 공유 게시판</h1>
 
+        <!-- 검색 폼 -->
+        <div id="search_box">
+            <form action="./search_result.php" method="get" onsubmit="return validateForm()">
+                <div class="mb-3">
+                    <select class="form-select" name="catgo">
+                        <option value="title">제목</option>
+                        <option value="username">글쓴이</option>
+                        <option value="board">내용</option>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <input class="form-control" type="text" name="search" placeholder="검색어" required="required" />
+                </div>
+                <div class="mb-3">
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" name="category[]" value="freeboard">
+                        <label class="form-check-label">자유게시판</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" name="category[]" value="notification">
+                        <label class="form-check-label">공지사항</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" name="category[]" value="QandA">
+                        <label class="form-check-label">Q&A</label>
+                    </div>
+                </div>
+                <button class="btn btn-primary" type="submit">검색</button>
+            </form>
+        </div>
 
+        <!-- 게시판 테이블 -->
         <table class="table">
             <thead>
                 <tr>
@@ -167,49 +133,31 @@ switch ($sort) {
                 <?php } ?>
             </tbody>
         </table>
-        <div id="search_box">
-            <form action="./search_result.php" method="get" onsubmit="return validateForm()">
-                <select name="catgo">
-                    <option value="title">제목</option>
-                    <option value="username">글쓴이</option>
-                    <option value="board">내용</option>
-                </select>
-                <input type="text" name="search" size="40" required="required" />
+    </div>
 
-                <label><input type="checkbox" name="category[]" value="freeboard"> 자유게시판</label>
-                <label><input type="checkbox" name="category[]" value="notification"> 공지사항</label>
-                <label><input type="checkbox" name="category[]" value="QandA"> QandA</label>
+    <script>
+        function validateForm() {
+            // 체크박스들을 선택
+            var checkboxes = document.querySelectorAll('input[type="checkbox"][name="category[]"]');
+            var isChecked = false;
 
-                <button>검색</button>
-            </form>
-        </div>
-
-        <script>
-            function validateForm() {
-                // 체크박스들을 선택
-                var checkboxes = document.querySelectorAll('input[type="checkbox"][name="category[]"]');
-                var isChecked = false;
-
-                // 하나라도 체크되었는지 확인
-                checkboxes.forEach(function (checkbox) {
-                    if (checkbox.checked) {
-                        isChecked = true;
-                    }
-                });
-
-                // 체크가 되지 않았을 때 경고창 출력 후 검색 취소
-                if (!isChecked) {
-                    alert("하나 이상의 카테고리를 선택해주세요.");
-                    return false;
+            // 하나라도 체크되었는지 확인
+            checkboxes.forEach(function (checkbox) {
+                if (checkbox.checked) {
+                    isChecked = true;
                 }
+            });
 
-                // 체크가 되었을 때 폼 제출
-                return true;
+            // 체크가 되지 않았을 때 경고창 출력 후 검색 취소
+            if (!isChecked) {
+                alert("하나 이상의 카테고리를 선택해주세요.");
+                return false;
             }
-        </script>
 
-
-
+            // 체크가 되었을 때 폼 제출
+            return true;
+        }
+    </script>
 </body>
 
 </html>
