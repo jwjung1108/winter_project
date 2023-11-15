48bdcc6f0b92f8607d08d6f8612cb148f5b3aa03
jwjung1108 <jwjung1108@naver.com>
2023-11-15 16:33:32 +0900

    ㅁ

--- adminPage/managerBoard.php
+++ adminPage/managerBoard.php
@@ -1,6 +1,65 @@
-<?php
-    ?>
-    this is managerBoard Page 
-    <?php
+<!DOCTYPE html>
+<html lang="en">
 
-?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    this is Managerboard Page!!
+    <?php
+    include '../connect.php';
+
+    $sql = 'select * from board';
+    $result = mysqli_query($conn, $sql)
+        ?>
+    <div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">번호</th>
+                    <th scope="col">제목</th>
+                    <th scope="col">작성자</th>
+                    <th scope="col">등록일</th>
+                    <th scope="col">조회수</th>
+                    <th scope="col">추천수</th>
+                    <th scope="col">삭제</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $i = 1;
+                while ($row = mysqli_fetch_array($result)) {
+                    ?>
+                    <tr>
+                        <th scope="row">
+                            <?php echo $i++; ?>
+                        </th>
+                        <td>
+                            <?php echo $row['title']; ?>
+                        </td>
+                        <td>
+                            <?php echo $row['username']; ?>
+                        </td>
+                        <td>
+                            <?php echo $row['created']; ?>
+                        </td>
+                        <td>
+                            <?php echo $row['views']; ?>
+                        </td>
+                        <td>
+                            <?php echo $row['likes']; ?>
+                        </td>
+                        <td>
+                            <?php echo "삭제"; ?>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>
