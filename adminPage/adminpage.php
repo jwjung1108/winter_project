bb9eb755f0c7c8bd02b45608c91c4df8cc30ee05
jwjung1108 <jwjung1108@naver.com>
2023-11-15 15:46:38 +0900

    a

--- adminPage/adminpage.php
+++ adminPage/adminpage.php
@@ -12,23 +12,37 @@ include '../connect.php';
 
 <body>
     <div>
-        <button class="custom-button" onclick="executeCommand(1)">버튼 1</button>
+        <button class="custom-button" onclick="goManagerBoardPage()">버튼 1</button>
         <button class="custom-button" onclick="executeCommand(2)">버튼 2</button>
         <button class="custom-button" onclick="executeCommand(3)">버튼 3</button>
         <button class="custom-button" onclick="executeCommand(4)">버튼 4</button>
         <button class="custom-button" onclick="executeCommand(5)">버튼 5</button>
     </div>
+    <?php
+        $sql = ''    
+    ?>
 
     <script>
         // 클릭 시 실행될 명령
+        function goManagerBoardPage(){
+            window.location.href = "/adminPage/managerBoard.php";
+        }
+
+
         function executeCommand(buttonNumber) {
             // 여기에 버튼을 클릭했을 때 실행될 특정 명령을 넣으세요.
-            alert('버튼 ' + buttonNumber + '이(가) 클릭되었습니다.');
-            // 버튼 1부터 5까지 각각 다른 명령을 실행하도록 원하시는 작업을 여기에 추가하세요.
+            // alert('버튼 ' + buttonNumber + '이(가) 클릭되었습니다.');
+            // 버튼 1부터 5까지 각각 다른 명령을 실행하도록 원하시/는 작업을 여기에 추가하세요.
             // 예를 들어, switch 문을 사용하여 각 버튼별로 다른 동작을 수행하도록 구현할 수 있습니다.
             switch (buttonNumber) {
                 case 1:
                     // 버튼 1을 클릭했을 때 수행할 작업
+                    <?php
+                        $sql = 'select * from board';
+                    ?>
+
+
+
                     break;
                 case 2:
                     // 버튼 2를 클릭했을 때 수행할 작업
---
+++ adminPage/managerBoard.php
@@ -0,0 +1,6 @@
+<?php
+    ?>
+    this is managerBoard Page 
+    <?php
+
+?>
