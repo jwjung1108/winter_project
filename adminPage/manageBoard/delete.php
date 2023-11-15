219509c0b7a01fa0f583c4fa2a2250e483262506
jwjung1108 <jwjung1108@naver.com>
2023-11-15 16:49:23 +0900

    a

--- adminPage/manageBoard/delete.php
+++ adminPage/manageBoard/delete.php
@@ -1,8 +1,11 @@
 <?php
 include '../../connect.php';
+session_start();
 ?>
 
 <?php
+// 사용자 권한 확인
+$userId = isset($_SESSION['userId']) ? $_SESSION['userId'] : '';
 
 $sql = "SELECT authority FROM users WHERE id='$userId'";
 $row = mysqli_fetch_array(mysqli_query($conn, $sql));
@@ -18,6 +21,10 @@ $row = mysqli_fetch_array(mysqli_query($conn, $sql));
 <body>
     <?php
     $number = $_GET['number'];
+    $check_user = "SELECT username FROM board WHERE username = '$userId' AND number = '$number'";
+    $result = mysqli_fetch_array(mysqli_query($conn, $check_user));
+
+    if ($userId != $result['username']) {
         if ($row['authority'] != 2) {
             ?>
             <script>
@@ -34,8 +41,20 @@ $row = mysqli_fetch_array(mysqli_query($conn, $sql));
         alert("게시글이 삭제되었습니다.");
         location.href = "managerBoard.php";
     </script>
-
-
+    <?php
+    ?>
+    <?php
+    } else {
+        $sql = "UPDATE board SET visible = 0 WHERE number = '$number'";
+        mysqli_query($conn, $sql);
+        ?>
+        <script>
+            alert("게시글이 삭제되었습니다.");
+            location.href = "managerBoard.php";
+        </script>
+        <?php
+    }
+    ?>
 </body>
 
 </html>
