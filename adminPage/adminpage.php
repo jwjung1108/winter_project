930a91537071eefb8b1acf7ed94cb0d195902479
jwjung1108 <jwjung1108@naver.com>
2023-11-15 15:29:43 +0900

    a

--- adminPage/adminpage.php
+++ adminPage/adminpage.php
@@ -8,113 +8,35 @@ include '../connect.php';
 <html lang="ko">
 
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-        crossorigin="anonymous"></script>
-
-    <title>노하우 전수 블로그</title>
-    <style>
-        /* 추가한 스타일은 여기에 넣어주세요 */
-
-        /* 반응형 디자인 */
-        @media (max-width: 768px) {
-            /* 스타일 추가 */
-        }
-    </style>
 </head>
 
 <body>
-    <div class="container">
-        <h1 class="text-center">노하우 전수 블로그</h1>
-
-        <div id="search_box">
-            <form action="./search_result.php" method="get" onsubmit="return validateForm()">
-                <select name="catgo">
-                    <option value="title">제목</option>
-                    <option value="username">글쓴이</option>
-                    <option value="board">내용</option>
-                </select>
-                <input type="text" name="search" size="40" required="required" />
-
-                <label><input type="checkbox" name="category[]" value="freeboard"> 자유게시판</label>
-                <label><input type="checkbox" name="category[]" value="notification"> 공지사항</label>
-                <label><input type="checkbox" name="category[]" value="QandA"> Q&A</label>
-
-                <button>검색</button>
-            </form>
-        </div>
-
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">번호</th>
-                    <th scope="col">제목</th>
-                    <th scope="col">작성자</th>
-                    <th scope="col">등록일</th>
-                    <th scope="col">조회수</th>
-                    <th scope="col">추천수</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                $i = 1;
-                while ($row = mysqli_fetch_array($result)) {
-                    ?>
-                    <tr>
-                        <th scope="row">
-                            <?php echo $i++; ?>
-                        </th>
-                        <td><a href="readBoard.php?number=<?php echo $row['number']; ?>"><?php echo $row['title']; ?></a>
-                        </td>
-                        <td>
-                            <?php echo $row['username']; ?>
-                        </td>
-                        <td>
-                            <?php echo $row['created']; ?>
-                        </td>
-                        <td>
-                            <?php echo $row['views']; ?>
-                        </td>
-                        <td>
-                            <?php echo $row['likes']; ?>
-                        </td>
-                    </tr>
-                <?php } ?>
-            </tbody>
-        </table>
-    </div>
+    <button class="custom-button" onclick="executeCommand(1)">버튼 1</button>
+    <button class="custom-button" onclick="executeCommand(2)">버튼 2</button>
+    <button class="custom-button" onclick="executeCommand(3)">버튼 3</button>
+    <button class="custom-button" onclick="executeCommand(4)">버튼 4</button>
+    <button class="custom-button" onclick="executeCommand(5)">버튼 5</button>
 
     <script>
-        function validateForm() {
-            // 체크박스들을 선택
-            var checkboxes = document.querySelectorAll('input[type="checkbox"][name="category[]"]');
-            var isChecked = false;
-
-            // 하나라도 체크되었는지 확인
-            checkboxes.forEach(function (checkbox) {
-                if (checkbox.checked) {
-                    isChecked = true;
-                }
-            });
-
-            // 체크가 되지 않았을 때 경고창 출력 후 검색 취소
-            if (!isChecked) {
-                alert("하나 이상의 카테고리를 선택해주세요.");
-                return false;
+        // 클릭 시 실행될 명령
+        function executeCommand(buttonNumber) {
+            // 여기에 버튼을 클릭했을 때 실행될 특정 명령을 넣으세요.
+            console.log('버튼 ' + buttonNumber + '이(가) 클릭되었습니다.');
+            // 버튼 1부터 5까지 각각 다른 명령을 실행하도록 원하시는 작업을 여기에 추가하세요.
+            // 예를 들어, switch 문을 사용하여 각 버튼별로 다른 동작을 수행하도록 구현할 수 있습니다.
+            switch (buttonNumber) {
+                case 1:
+                    // 버튼 1을 클릭했을 때 수행할 작업
+                    break;
+                case 2:
+                    // 버튼 2를 클릭했을 때 수행할 작업
+                    break;
+                // 이하 버튼 3, 4, 5에 대한 작업도 추가할 수 있습니다.
+                default:
+                    break;
             }
-
-            // 체크가 되었을 때 폼 제출
-            return true;
         }
     </script>
 </body>
 
 </html>
-
--- index.php
+++ index.php
@@ -6,36 +6,270 @@ session_start();
 <html>
 
 <head>
-    <bod>
+    <meta charset="UTF-8">
+    <title>홈페이지</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css"
+        integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.bundle.min.js"
+        integrity="sha384-BOsAfwzjNJHrJ8cZidOg56tcQWfp6y72vEJ8xQ9w6Quywb24iOsW913URv1IS4GD"
+        crossorigin="anonymous"></script>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f2f2f2;
+        }
 
-        <button class="custom-button" onclick="executeCommand(1)">버튼 1</button>
-        <button class="custom-button" onclick="executeCommand(2)">버튼 2</button>
-        <button class="custom-button" onclick="executeCommand(3)">버튼 3</button>
-        <button class="custom-button" onclick="executeCommand(4)">버튼 4</button>
-        <button class="custom-button" onclick="executeCommand(5)">버튼 5</button>
+        .container {
+            width: 80%;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+            color: #000;
+            /* 글자 색상을 검정으로 변경 */
+        }
 
-        <script>
-            // 클릭 시 실행될 명령
-            function executeCommand(buttonNumber) {
-                // 여기에 버튼을 클릭했을 때 실행될 특정 명령을 넣으세요.
-                console.log('버튼 ' + buttonNumber + '이(가) 클릭되었습니다.');
-                // 버튼 1부터 5까지 각각 다른 명령을 실행하도록 원하시는 작업을 여기에 추가하세요.
-                // 예를 들어, switch 문을 사용하여 각 버튼별로 다른 동작을 수행하도록 구현할 수 있습니다.
-                switch (buttonNumber) {
-                    case 1:
-                        // 버튼 1을 클릭했을 때 수행할 작업
-                        break;
-                    case 2:
-                        // 버튼 2를 클릭했을 때 수행할 작업
-                        break;
-                    // 이하 버튼 3, 4, 5에 대한 작업도 추가할 수 있습니다.
-                    default:
-                        break;
-                }
+        h1 {
+            text-align: center;
+            color: #000000;
+            margin-bottom: 30px;
+        }
+
+        .button-container {
+            text-align: center;
+            margin: 20px 0;
+        }
+
+        .button-container button {
+            margin-right: 10px;
+            padding: 10px 20px;
+            font-size: 16px;
+            font-weight: bold;
+            border: none;
+            color: #fff;
+            background-color: #007bff;
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+            border-radius: 5px;
+        }
+
+        .button-container button:hover {
+            background-color: #0056b3;
+        }
+
+        .welcome-container {
+            text-align: right;
+            margin-bottom: 20px;
+        }
+
+        .welcome-message {
+            font-weight: bold;
+            margin-right: 10px;
+        }
+
+        .logout-button {
+            cursor: pointer;
+            color: #007bff;
+            text-decoration: underline;
+        }
+
+        .board-buttons {
+            display: flex;
+            justify-content: center;
+            margin-top: 30px;
+            margin-bottom: 30px;
+        }
+
+        .notification-button {
+            display: flex;
+            justify-content: center;
+            margin-bottom: 30px;
+        }
+        .navbar-custom {
+    background-color: #0056b3;
+    padding: 10px 0;
+    color: white;
+}
+
+.nav-link,
+.navbar-brand {
+    color: white;
+    margin-right: 15px;
+}
+
+.nav-link:hover,
+.navbar-brand:hover {
+    color: #f2f2f2;
+}
+
+/* 섹션 타이틀 스타일링 */
+.section-title {
+    margin-top: 20px;
+    margin-bottom: 10px;
+    color: #333;
+    text-align: left;
+    font-size: 24px;
+    font-weight: 600;
+}
+
+/* 카드 스타일링 */
+.card-custom {
+    margin: 10px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+}
+
+.card-custom img {
+    width: 100%;
+    height: auto;
+    border-top-left-radius: calc(.25rem - 1px);
+    border-top-right-radius: calc(.25rem - 1px);
+}
+
+.card-body-custom {
+    padding: 10px;
+}
+
+.card-title {
+    margin-bottom: 10px;
+    font-weight: bold;
+}
+
+.card-text {
+    font-size: 14px;
+    color: #666;
+}
+
+.card-link {
+    font-size: 16px;
+    color: #0056b3;
+}
+
+.card-link:hover {
+    text-decoration: none;
+    color: #003d82;
+}
+    </style>
+    <script>
+        function goToLoginPage() {
+            window.location.href = "/join/login.php";
+        }
+        function goToSignupPage() {
+            window.location.href = "/join/signup.php";
+        }
+        function goTocommonBoardPage() {
+            window.location.href = "/board/list_board.php";
+        }
+        function goTonotificationBoardPage() {
+            window.location.href = "/board/notification/list_nboard.php";
+        }
+        function logout() {
+            const data = confirm("로그아웃 하시겠습니까?");
+            if (data) {
+                location.href = "/join/logoutProcess.php";
             }
-        </script>
+        }
+        function goToadminPage() {
+            window.location.href = "/adminPage/adminpage.php";
+        }
+    </script>
+</head>
+
+<body>
+
+ <!--- GPT 코드 주석--->
+
+<nav class="navbar navbar-custom">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">학습  커뮤니티</a>
+        <div class="d-flex align-items-center">
+            <?php if (isset($_SESSION['userId'])) { ?>
+                <span class="navbar-text mr-3">
+                    <?php echo $_SESSION['userId']; ?> 님 환영합니다
+                </span>
+                <button class="btn btn-outline-light" onclick="logout()">로그아웃</button>
+                <?php if($_SESSION['authority'] == 'admin'){ ?>
+                <button class="btn btn-outline-light" onclick="goToadminPage()">관리자페이지</button>
+                <?php }?>
+            <?php } else { ?>
+                <button class="btn btn-outline-light" onclick="goToLoginPage()">로그인</button>
+                <button class="btn btn-outline-light" onclick="goToSignupPage()">회원가입</button>
+
+            <?php } ?>
+        </div>
+    </div>
+</nav>
 
 
-        </body>
+<!-- Main Content -->
+<div class="container mt-4">
+
+    <!-- Section Title -->
+    <div class="section-title">
+        메뉴
+    </div>
+    
+    <!-- Features -->
+    <div class="row">
+        <!-- 공지 사항 -->
+        <div class="col-md-3">
+            <div class="card card-custom">
+                <img src="/image/notice.png" alt="공지 사항">
+                <div class="card-body card-body-custom ">
+                    <h5 class="card-title">공지사항</h5>
+                    <p class="card-text">공지사항 읽어라.</p>
+                    <button onclick="goTonotificationBoardPage()">공지사항</button>
+                </div>
+            </div>
+        </div>
+
+        <!-- 자유 게시판 -->
+        <div class="col-md-3">
+            <div class="card card-custom">
+                <img src="/image/tip.png" alt="자유게시판">
+                <div class="card-body card-body-custom">
+                    <h5 class="card-title">자유게시판</h5>
+                    <p class="card-text">자유게시판 사용해라.</p>
+                    <?php if (isset($_SESSION['userId'])) { ?>
+                <button onclick="goTocommonBoardPage()">자유게시판</button>
+            <?php } ?>
+                </div>
+            </div>
+        </div>
+        
+        <!-- 자료실 -->
+        <div class="col-md-3">
+            <div class="card card-custom">
+                <img src="/image/resource.png" alt="resouce">
+                <div class="card-body card-body-custom">
+                    <h5 class="card-title">자료실</h5>
+                    <p class="card-text">좋은 자료가 많다.</p>
+                    
+                </div>
+            </div>
+        </div>
+        
+        <!-- Q&A -->
+        <div class="col-md-3">
+            <div class="card card-custom">
+                <img src="/image/Q&A.png" alt="Q&A">
+                <div class="card-body card-body-custom">
+                    <h5 class="card-title">Q&A</h5>
+                    <p class="card-text">묻고 답하라.</p>
+                    
+                </div>
+            </div>
+        </div>
+        
+        
+    </div>
+</div>
+
+
+
+
+</body>
 
 </html>
