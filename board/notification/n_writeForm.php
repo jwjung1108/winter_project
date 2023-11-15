9ffaaf1a30ae4fd56cbfbdfcaddf4f5813719405
jwjung1108 <jwjung1108@naver.com>
2023-11-15 17:59:19 +0900

    a

--- board/notification/n_writeForm.php
+++ board/notification/n_writeForm.php
@@ -2,9 +2,23 @@
 session_start();
 $userId = isset($_SESSION['userId']) ? $_SESSION['userId'] : '';
 
-$sql = "select authority from users where id='$userId'";
-$row = mysqli_fetch_array(mysqli_query($conn, $sql));
-if($row['authority'] != 2){
+// 이전에 DB 연결을 정의한 부분이 필요합니다.
+// $conn = mysqli_connect("호스트", "사용자이름", "비밀번호", "데이터베이스이름");
+
+// 2. SQL Injection 방지를 위해 Prepared Statements 사용
+$stmt = mysqli_prepare($conn, "SELECT authority FROM users WHERE id = ?");
+mysqli_stmt_bind_param($stmt, "s", $userId);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+
+if (!$result) {
+    // 쿼리 실행 실패
+    exit("쿼리 실행에 실패했습니다.");
+}
+
+$row = mysqli_fetch_array($result);
+
+if ($row['authority'] != 2) {
     ?>
     <script>
         alert("지정된 사용자가 아닙니다.");
@@ -15,7 +29,6 @@ if($row['authority'] != 2){
 }
 ?>
 
-
 <!DOCTYPE html>
 <html lang="ko">
 
@@ -24,9 +37,9 @@ if($row['authority'] != 2){
 
 <body>
     <form action="n_saveBoard.php" method="POST" enctype="multipart/form-data">
-        <p><input type="title" name="title" placeholder="제목"></p>
+        <p><input type="text" name="title" placeholder="제목"></p>
         <p><textarea name="board" placeholder="본문"></textarea></p>
-        <input type="file" name="file" >
+        <input type="file" name="file">
         <p><input type="submit" value="작성"></p>
     </form>
 </body>
