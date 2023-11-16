b9cca5c850043fd474e524eb687b948b10c8bba7
jwjung1108 <jwjung1108@naver.com>
2023-11-16 19:00:52 +0900

    a

--- adminPage/manageUser/delete_user.php
+++ adminPage/manageUser/delete_user.php
@@ -5,24 +5,19 @@ include '../check_admin.php';
 
 <?php
 $userid = $_GET['id'];
-
-echo "<script>";
-echo "if(confirm('사용자를 삭제하시겠습니까?')) {";
-$sql = "delete from users where id = '$userid'";
-$result = mysqli_query($conn, $sql);
 ?>
 <script>
-    alert("<?php echo "$userid" ?> 유저가 삭제되었습니다.");
-    location.href = 'managerUsers.php';
+    if (confirm('사용자를 삭제하시겠습니까?')) {
 </script>
-<?php
-echo "} else {";
-echo "  window.location.href = 'managerUsers.php';";
-echo "}";
-echo "</script>";
-
-
-
-
-
-?>
+    <?php
+    $sql = "delete from users where id = '$userid'";
+    $result = mysqli_query($conn, $sql);
+    ?>
+<script>
+        alert("<?php echo "$userid" ?> 유저가 삭제되었습니다.");
+        location.href = 'managerUsers.php';
+</script>
+}
+else{
+    windows.location.href = 'managerUsers.php';
+}
