15edf86a1ef0736f3751136548398d17b287ddbe
sharesugi <family6813@naver.com>
2023-11-19 18:48:35 +0900

    test.commit

--- board/notification/n_saveBoard.php
+++ board/notification/n_saveBoard.php
@@ -3,29 +3,12 @@ session_start();
 $userId = isset($_SESSION['userId']) ? $_SESSION['userId'] : '';
 include '../../connect.php';
 
-
-$sql = "select authority from users where id='$userId'";
-$row = mysqli_fetch_array(mysqli_query($conn, $sql));
-if($row['authority'] != 2){
-    ?>
-    <script>
-        alert("지정된 사용자가 아닙니다.");
-        location.href = "./list_nboard.php";
-    </script>
-    <?php
-    exit();
-}
-
-
-
 $view = 0;
 $like = 0;
-
 $title = $_POST['title'];
 $board = $_POST['board'];
 
 $fileDestination = '';
-
 $file = $_FILES['file'];
 
 // 파일 정보 가져오기
@@ -34,10 +17,8 @@ $fileTmpName = $file['tmp_name'];
 $fileSize = $file['size'];
 $fileType = $file['type'];
 
-
 // 파일 저장 경로 설정  
 $uploadDir = '/home/upload/notification/';
-
 
 // 파일 확장자 추출
 $fileExtension = pathinfo($fileType, PATHINFO_EXTENSION);
@@ -47,13 +28,13 @@ $fileSaveName = uniqid() . '.' . $fileExtension;
 
 // 파일을 지정된 경로로 이동
 move_uploaded_file($fileTmpName, $uploadDir . $fileSaveName);
-// var_dump($result);
 $fileDestination = $uploadDir . $fileSaveName;
 
-if ($fileName == "")
+if ($fileName == "") {
     $fileDestination = "";
+}
 
-// // 파일 업로드 처리
+// 파일 업로드 처리
 if (!move_uploaded_file($fileTmpName, $uploadDir . $fileSaveName)) {
     // 파일 업로드 성공한 경우
     $sql = "
@@ -67,9 +48,26 @@ if (!move_uploaded_file($fileTmpName, $uploadDir . $fileSaveName)) {
     if ($result === false) {
         echo "저장에 문제가 생겼습니다. 관리자에게 문의해주세요.";
     } else {
+        // 등급 업데이트
+        $sqlUpdateGrade = "UPDATE users SET grade = CASE 
+            WHEN grade = 'white' AND (SELECT COUNT(*) FROM board WHERE username = '$userId') >= 2 THEN 'silver'
+            WHEN grade = 'silver' AND (SELECT COUNT(*) FROM board WHERE username = '$userId') >= 4 THEN 'gold'
+            ELSE grade
+            END
+            WHERE id = '$userId'";
+        mysqli_query($conn, $sqlUpdateGrade);
+
+        // 등급에 따른 표현 추가
+        $sqlSelectGrade = "SELECT grade FROM users WHERE id = '$userId'";
+        $resultGrade = mysqli_query($conn, $sqlSelectGrade);
+        $rowGrade = mysqli_fetch_assoc($resultGrade);
+        $newGrade = $rowGrade['grade'];
+
+        $userDisplayName = "$userId($newGrade)";
+
         ?>
         <script>
-            alert("게시글이 작성되었습니다.");
+            alert("게시글이 작성되었습니다. 사용자 등급: <?php echo $userDisplayName; ?>");
             location.href = "./list_nboard.php";
         </script>
         <?php
