cd5096122743f930e1bf127fbcf9f9e56cdcf96a
underkim <rlaehdgns4568@gmail.com>
2023-11-20 14:03:58 +0900

    마이페이지 쓰껄

--- MyPage/mypage.php
+++ MyPage/mypage.php
@@ -21,34 +21,40 @@ if ($userId == '') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>마이페이지</title>
-    <link rel="stylesheet" href="../css/mypage_style.css"> 
+    <link rel="stylesheet" href="../css/mypage_style.css">
 </head>
 
 <body>
-<button onclick="goBack()" class="back-button">이전 페이지로 돌아가기</button>
 
-<script>
-    function goBack() {
-        window.history.back();
-    }
-</script>
+    <div class="header">
+        <button onclick="goBack()" class="back-button">이전</button>
+    </div>
+
+    <script>
+        function goBack() {
+            window.history.back();
+        }
+    </script>
 
     <div class="container">
         <h1>마이페이지에 오신 것을 환영합니다.</h1>
-        <h2>어서오세요 <?php echo $userId; ?>님 </h2>
-        
-        <?php
-            $sql = "select * from users where id = '$userId'";
-            $result = mysqli_fetch_array(mysqli_query($conn, $sql));
+        <h2>어서오세요
+            <?php echo $userId; ?>님
+        </h2>
 
-            echo "<div class='user-info'>";
-            echo "<p>아이디 : " . $result['id'] . "</p>";
-            echo "<p>닉네임 : " . $result['nickname'] . "</p>";
-            echo "<p>이메일 : " . $result['email'] . "</p>";
-            echo "<p>랭크 : " . $result['user_rank'] . "</p>";
-            echo "<p>포인트 : " . $result['point'] . "</p>";
-            echo "</div>";
+        <?php
+        $sql = "select * from users where id = '$userId'";
+        $result = mysqli_fetch_array(mysqli_query($conn, $sql));
+
+        echo "<div class='user-info'>";
+        echo "<p>아이디 : " . $result['id'] . "</p>";
+        echo "<p>닉네임 : " . $result['nickname'] . "</p>";
+        echo "<p>이메일 : " . $result['email'] . "</p>";
+        echo "<p>랭크 : " . $result['user_rank'] . "</p>";
+        echo "<p>포인트 : " . $result['point'] . "</p>";
+        echo "</div>";
         ?>
     </div>
 </body>
+
 </html>
