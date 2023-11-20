feaf92a08877dde80f1500fa1a8bcfd5dc22d7d9
jwjung1108 <jwjung1108@naver.com>
2023-11-20 14:12:16 +0900

    a

--- board/point/WriteCoPoint.php
+++ board/point/WriteCoPoint.php
@@ -15,7 +15,7 @@ if ($userId == '') {
 $sql = "select point from users where id = '$userId'";
 $result = mysqli_fetch_array(mysqli_query($conn, $sql));
 
-$point = $result['point'] + 5;
+$point = $result['point'] + 4;
 
 $sql = "update users set point = '$point' where id = '$userId'";
 mysqli_query( $conn, $sql);
