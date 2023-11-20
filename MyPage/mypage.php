ea74b7326461b78ffa37d8f1c37be655b2660d38
underkim <rlaehdgns4568@gmail.com>
2023-11-20 13:53:29 +0900

    mypage css

--- MyPage/mypage.php
+++ MyPage/mypage.php
@@ -21,6 +21,7 @@ if ($userId == '') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>마이페이지</title>
+    <link rel="stylesheet" href="/css/mypage_style.css"> 
 </head>
 
 <body>
