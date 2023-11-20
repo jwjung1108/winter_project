0340425255746c2fee5c8d2814cd835b2a717309
jwjung1108 <jwjung1108@naver.com>
2023-11-20 12:59:13 +0900

    a

--- adminPage/manageUser/change_rank.php
+++ adminPage/manageUser/change_rank.php
@@ -5,21 +5,21 @@ include '../check_admin.php';
 $userid = isset($_GET['user']) ? $_GET['user'] : '';
 $rank = isset($_GET['rank']) ? $_GET['rank'] : '';
 
-$sql = 'select * from users';
+$sql = "select * from users where id = '$userid'";
 $result = mysqli_fetch_array(mysqli_query($conn, $sql));
 
-if($result['id'] === "$userid"){
-    ?>
-    <script>
-        alert('사용자가 존재하지 않습니다.');
-        location.href='managerUsers.php';
-    </script>
-    <?php
-}
-else{
+if ($result) {
     echo "Hello World!";
     echo "$userid";
     echo "$rank";
+} else {
+    ?>
+    <script>
+        alert('사용자가 존재하지 않습니다.');
+        location.href = 'managerUsers.php';
+    </script>
+    <?php
+
 }
 
 
