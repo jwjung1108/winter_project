b0d3290fdad0686acb675e3c03062a28c83e2f85
jwjung1108 <jwjung1108@naver.com>
2023-11-20 15:19:46 +0900

    a

--- board/reference/r_readBoard.php
+++ board/reference/r_readBoard.php
@@ -191,7 +191,7 @@ include '../point/ReadPoint.php';
                 echo "다운로드 파일이 존재하지 않습니다.";
             } else {
                 echo $board['filename'] . " "; ?>
-                <a href="../download.php?number=<?php echo $board['number']; ?>">[다운로드]</a>
+                <a href="../r_download.php?number=<?php echo $board['number']; ?>">[다운로드]</a>
                 <?php
             }
             ?>
