bfe26dfee077424cccd2f3a4d2670318d2d0c049
jwjung1108 <jwjung1108@naver.com>
2023-11-20 15:19:19 +0900

    a

---
+++ board/r_download.php
@@ -0,0 +1,25 @@
+<?php
+include '../connect.php';
+
+$number = $_GET['number'];
+
+$sql = "select * from reference where number = $number";
+$result = mysqli_fetch_array(mysqli_query($conn,$sql));
+$filepath = $result['filepath'];
+$filesize = filesize($filepath);
+$path_parts = pathinfo($filepath);
+// $filename = $path_parts['basename'];
+$filename = $result['filename'];
+$extension = $path_parts['extension'];
+
+header("Pragma: public");
+header("Expires: 0");
+header("Content-Type: application/octet-stream");
+header("Content-Disposition: attachment; filename=$filename");
+header("Content-Transfer-Encoding: binary");
+header("Content-Length: $filesize");
+
+ob_clean();
+flush();
+readfile($filepath);
+?>
