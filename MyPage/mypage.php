b16f407fa7a2c4f60a2db054728295bc78c3d8fb
jwjung1108 <jwjung1108@naver.com>
2023-11-20 13:29:18 +0900

    a

--- MyPage/mypage.php
+++ MyPage/mypage.php
@@ -12,8 +12,34 @@ if($userId == ''){
     <?php
     exit();
 }
+?>
 
-echo "Hello World!";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>마이페이지</title>
+</head>
+<body>
+    <h1>마이페이지에 오신 것을 환영합니다.</br>
+    <h2>어서오세요 <?php echo $userId; ?>님 </br>
+</body>
+</html>
+
+<?php
+
+
+
+
+$sql = "select * from users where id = '$userId'";
+
+
+
+
+
+
+
 
 
 
