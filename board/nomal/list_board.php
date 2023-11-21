32a3a808c852baf0cbd070dff4cfd5622ec95a94
jwjung1108 <jwjung1108@naver.com>
2023-11-21 12:07:44 +0900

    a

--- board/nomal/list_board.php
+++ board/nomal/list_board.php
@@ -36,18 +36,6 @@ $result = mysqli_query($conn, $sql);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- script -->
-    <script src='../js/checkbox.js'></script>
-
-    <script>
-        function logout() {
-            const data = confirm("로그아웃 하시겠습니까?");
-            if (data) {
-                location.href = "../../join/logoutProcess.php";
-            }
-        } 
-    </script>
-
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -78,6 +66,17 @@ $result = mysqli_query($conn, $sql);
 </head>
 
 <body>
+    <script src='../js/checkbox.js'></script>
+
+    <script>
+        function logout() {
+            const data = confirm("로그아웃 하시겠습니까?");
+            if (data) {
+                location.href = "../../join/logoutProcess.php";
+            }
+        } 
+    </script>
+
     <nav class="navbar navbar-expand-lg navbar-custom fixed-top">
         <div class="container-fluid">
             <!-- Navbar Brand -->
