cf687082acc628b302945ea3a38a87088278d0e4
sharesugi <family6813@naver.com>
2023-11-21 15:23:20 +0900

    l

--- board/notification/n_saveBoard.php
+++ board/notification/n_saveBoard.php
@@ -3,29 +3,31 @@ session_start();
 $userId = isset($_SESSION['userId']) ? $_SESSION['userId'] : '';
 include '../../connect.php';
 
-
-$sql = "select authority from users where id='$userId'";
-$row = mysqli_fetch_array(mysqli_query($conn, $sql));
-if ($row['authority'] != 2) {
+if ($userId == '') {
     ?>
     <script>
-        alert("지정된 사용자가 아닙니다.");
-        location.href = "./list_nboard.php";
+        alert("로그인을 해주세요.");
+        location.href = "./list_qboard.php";
     </script>
     <?php
     exit();
 }
 
 
-
 $view = 0;
 $like = 0;
+
 $title = $_POST['title'];
 $board = $_POST['board'];
-$important = isset($_POST['important']) ? 1 : 0;
+$isSecret = $_POST['isSecret'];
+
+if ($isSecret == NULL) {
+    $isSecret = 0;
+}
 
 
 $fileDestination = '';
+
 $file = $_FILES['file'];
 
 // 파일 정보 가져오기
@@ -34,8 +36,10 @@ $fileTmpName = $file['tmp_name'];
 $fileSize = $file['size'];
 $fileType = $file['type'];
 
+
 // 파일 저장 경로 설정  
-$uploadDir = '/home/upload/notification/';
+$uploadDir = '/home/upload/QandA/';
+
 
 // 파일 확장자 추출
 $fileExtension = pathinfo($fileType, PATHINFO_EXTENSION);
@@ -45,19 +49,19 @@ $fileSaveName = uniqid() . '.' . $fileExtension;
 
 // 파일을 지정된 경로로 이동
 move_uploaded_file($fileTmpName, $uploadDir . $fileSaveName);
+// var_dump($result);
 $fileDestination = $uploadDir . $fileSaveName;
 
-if ($fileName == "") {
+if ($fileName == "")
     $fileDestination = "";
-}
 
-// 파일 업로드 처리
+// // 파일 업로드 처리
 if (!move_uploaded_file($fileTmpName, $uploadDir . $fileSaveName)) {
     // 파일 업로드 성공한 경우
     $sql = "
         INSERT INTO board
-        (title, board, username, views, likes, created, visible, freeboard, notification, QandA, isSecret, filepath, filename, important)
-        VALUES ('$title', '$board', '$userId', '$view', '$like', NOW(), 1, 0, 1, 0, 0, '$fileDestination', '$fileName', '$important')
+        (title, board, username, views, likes, created, visible, freeboard, notification, QandA, isSecret, filepath, filename)
+        VALUES ('$title', '$board', '$userId', '$view', '$like', NOW(), 1, 0, 0, 1, '$isSecret', '$fileDestination', '$fileName')
     ";
 
     $result = mysqli_query($conn, $sql);
@@ -65,27 +69,10 @@ if (!move_uploaded_file($fileTmpName, $uploadDir . $fileSaveName)) {
     if ($result === false) {
         echo "저장에 문제가 생겼습니다. 관리자에게 문의해주세요.";
     } else {
-        // 등급 업데이트
-        $sqlUpdateGrade = "UPDATE users SET grade = CASE 
-            WHEN grade = 'white' AND (SELECT COUNT(*) FROM board WHERE username = '$userId') >= 2 THEN 'silver'
-            WHEN grade = 'silver' AND (SELECT COUNT(*) FROM board WHERE username = '$userId') >= 4 THEN 'gold'
-            ELSE grade
-            END
-            WHERE id = '$userId'";
-        mysqli_query($conn, $sqlUpdateGrade);
-
-        // 등급에 따른 표현 추가
-        $sqlSelectGrade = "SELECT grade FROM users WHERE id = '$userId'";
-        $resultGrade = mysqli_query($conn, $sqlSelectGrade);
-        $rowGrade = mysqli_fetch_assoc($resultGrade);
-        $newGrade = $rowGrade['grade'];
-
-        $userDisplayName = "$userId($newGrade)";
-
         ?>
         <script>
-            alert("게시글이 작성되었습니다. 사용자 등급: <?php echo $userDisplayName; ?>");
-            location.href = "./list_nboard.php";
+            alert("게시글이 작성되었습니다.");
+            location.href = "list_qboard.php";
         </script>
         <?php
     }
@@ -93,7 +80,7 @@ if (!move_uploaded_file($fileTmpName, $uploadDir . $fileSaveName)) {
     ?>
     <script>
         alert("파일 업로드에 실패하였습니다.");
-        location.href = "list_board.php";
+        location.href = "./list_qboard.php";
     </script>
     <?php
 }
