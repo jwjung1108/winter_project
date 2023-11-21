81b16e1880305af68872ece5a0cae06204a4cdd7
underkim <rlaehdgns4568@gmail.com>
2023-11-21 12:50:44 +0900

    마이페이지 꾸미기

--- MyPage/mypage.php
+++ MyPage/mypage.php
@@ -33,36 +33,37 @@ if ($userId == '') {
 
 
     <div class="container">
-        <h1>마이페이지에 오신 것을 환영합니다.</h1>
-        <h2>어서오세요
-            <?php echo $userId; ?>님
-        </h2>
+        <div>
+            <h1>마이페이지에 오신 것을 환영합니다.</h1>
+            <h2>어서오세요
+                <?php echo $userId; ?>님
+            </h2>
 
-        <?php
-        $sql = "select * from users where id = '$userId'";
-        $result = mysqli_fetch_array(mysqli_query($conn, $sql));
+            <?php
+            $sql = "select * from users where id = '$userId'";
+            $result = mysqli_fetch_array(mysqli_query($conn, $sql));
 
-        echo "<div class='user-info'>";
-        echo "<p>아이디 : " . $result['id'] . "</p>";
-        echo "<p>닉네임 : " . $result['nickname'] . "</p>";
-        echo "<p>이메일 : " . $result['email'] . "</p>";
-        echo "<p>랭크 : " . $result['user_rank'] . "</p>";
-        echo "<p>포인트 : " . $result['point'] . "</p>";
-
-
-
-        if ($result['user_rank'] != 'CH') {
-            echo "<button onclick='requestRankUp()' class='rank-up-button'>등급 업 신청</button>";
-        }
-
-        echo "</div>";
-        ?>
+            echo "<div class='user-info'>";
+            echo "<p>아이디 : " . $result['id'] . "</p>";
+            echo "<p>닉네임 : " . $result['nickname'] . "</p>";
+            echo "<p>이메일 : " . $result['email'] . "</p>";
+            echo "<p>랭크 : " . $result['user_rank'] . "</p>";
+            echo "<p>포인트 : " . $result['point'] . "</p>";
 
 
 
-    </div>
-    <div class="character">
-        <img src="/image/cha.webp" alt="Character Image">
+            if ($result['user_rank'] != 'CH') {
+                echo "<button onclick='requestRankUp()' class='rank-up-button'>등급 업 신청</button>";
+            }
+
+            echo "</div>";
+            ?>
+        </div>
+        <div class="character">
+            <img src="/image/cha.jpg" alt="Character Image">
+        </div>
+
+
     </div>
 
     <script>
