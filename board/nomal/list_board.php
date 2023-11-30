994fed7df3c00395d1fb1724f62dd081160e0160
underkim <rlaehdgns4568@gmail.com>
2023-11-30 13:51:15 +0900

    오류수정

--- board/nomal/list_board.php
+++ board/nomal/list_board.php
@@ -25,7 +25,7 @@ switch ($sort) {
 
 $sql = "SELECT board.*, users.user_rank 
         FROM board 
-        LEFT JOIN user ON board.username = users.nickname  
+        LEFT JOIN users ON board.username = users.nickname  
         WHERE board.visible = 1 AND board.notification = 0 AND board.QandA = 0 $orderBy";
 
 
