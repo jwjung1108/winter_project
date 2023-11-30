c4346399c1a8b4205dbc48826c7d65fd9925aece
underkim <rlaehdgns4568@gmail.com>
2023-11-30 14:08:43 +0900

    row 출력

--- board/nomal/list_board.php
+++ board/nomal/list_board.php
@@ -184,6 +184,7 @@ $result = mysqli_query($conn, $sql);
                     while ($row = mysqli_fetch_array($result)) {
                         $authorRank = $row['user_rank'];
                         print($authorRank);
+                        print($row);
                         // Determine color based on rank
                         switch ($authorRank) {
                             case 'bronze':
