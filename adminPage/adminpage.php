ea7cb9e7d2ffbe8659b9d3ac0724e5c1821362a2
underkim <rlaehdgns4568@gmail.com>
2024-01-14 00:10:22 +0900

    스크립트 제거

--- adminPage/adminpage.php
+++ adminPage/adminpage.php
@@ -9,10 +9,6 @@ include 'check_admin.php';
 <html lang="en" data-bs-theme="auto">
 
 <head>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
-
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
